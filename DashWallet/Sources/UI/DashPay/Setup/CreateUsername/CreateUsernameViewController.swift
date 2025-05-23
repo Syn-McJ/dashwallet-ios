a9639d886d543d2d816a66f29c7b7e771c165b54
Andrei Ashikhmin <synmcj@gmail.com>
2025-05-23 17:51:40 +0800

    fix: disable contested & UI fixes

--- DashWallet/Sources/UI/DashPay/Setup/CreateUsername/CreateUsernameViewController.swift
+++ DashWallet/Sources/UI/DashPay/Setup/CreateUsername/CreateUsernameViewController.swift
@@ -295,10 +295,6 @@ struct CreateUsernameView: View {
         .sheet(isPresented: $showContestedInfo) {
             let dialog = BottomSheet(showBackButton: Binding<Bool>.constant(false)) {
                 TextIntro(
-                    buttonLabel: NSLocalizedString("OK", comment: ""),
-                    action: {
-                        showContestedInfo = false
-                    },
                     inProgress: .constant(false)
                 ) {
                     FeatureTopText(
@@ -310,7 +306,7 @@ struct CreateUsernameView: View {
             }
             
             if #available(iOS 16.0, *) {
-                dialog.presentationDetents([.height(500)])
+                dialog.presentationDetents([.height(400)])
             } else {
                 dialog
             }
